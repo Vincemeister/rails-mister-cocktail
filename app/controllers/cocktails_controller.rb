d9606d68be1a8bf75c52b3b4aaff0f71d1efac16
Vincent Rehms <v.rehms@gmail.com>
2020-02-28 15:55:32 +0800

    added image uploading capabilities

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -30,7 +30,7 @@ private
   def cocktail_params
     # *Strong params*: You need to *whitelist* what can be updated by the user
     # Never trust user data!
-    params.require(:cocktail).permit(:name, :address)
+    params.require(:cocktail).permit(:name, :address, :photo)
   end
 
 end
